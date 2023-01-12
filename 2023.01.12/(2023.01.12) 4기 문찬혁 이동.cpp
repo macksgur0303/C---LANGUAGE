1cb7f18d302e0f3a5ac997db93c0a1470e8715bc
macksgur0303 <121844168+macksgur0303@users.noreply.github.com>
2023-01-12 13:24:43 +0900

    Add files via upload

---
+++ 2023.01.12/(2023.01.12) 4기 문찬혁 이동.cpp
@@ -0,0 +1,148 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void first_page(int a[][7], int b[][7]);
+void print_page(int a[][7]);
+void move_page(int a[][7], int b[][7]);
+
+int main()
+{
+	int a[7][7] = { 0 };
+	int b[7][7] = { 0 };
+
+	first_page(a, b);
+	print_page(a);
+	move_page(a, b);
+}
+
+void first_page(int a[][7], int b[][7]) // 여기를 깔끔하게 못함
+{
+	int i, j;
+	int n = 4;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 4; j < n + 1; j++)
+		{
+			a[i][7 - j] = 777;
+			a[i][j - 1] = 777;
+		}
+		n++;
+	}
+
+	for (i = 3; i < 7; i++)
+	{
+		for (j = 4; j < n + 1; j++)
+		{
+			a[i][7 - j] = 777;
+			a[i][j - 1] = 777;
+		}
+		n--;
+	}
+	n = 7;
+
+}
+
+void print_page(int a[][7])
+{
+	for (int i = 0; i < 7; i++)
+	{
+		for (int j = 0; j < 7; j++)
+		{
+			printf("\t%d", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void move_page(int a[][7], int b[][7])
+{
+	int choice = 0;
+
+	printf("이동 입력 오른쪽 = 1, 왼쪽 = 2, 위쪽 = 3, 아래쪽 = 4, 종료 = 5\n");
+	scanf("%d", &choice);
+
+	while (choice != 5)
+	{
+		if (choice == 1) // 오른쪽
+		{
+			for (int i = 0; i < 7; i++)
+			{
+				for (int j = 0; j < 7; j++)
+				{
+					b[i][(j + 1) % 7] = a[i][j];
+				}
+			}
+
+			for (int i = 0; i < 7; i++)
+			{
+				for (int j = 0; j < 7; j++)
+				{
+					a[i][j] = b[i][j];
+				}
+			}
+			print_page(a);
+		}
+
+		else if (choice == 2) // 왼쪽
+		{
+			for (int i = 0; i < 7; i++)
+			{
+				for (int j = 0; j < 7; j++)
+				{
+					b[i][j] = a[i][(j + 1) % 7];
+				}
+			}
+
+			for (int i = 0; i < 7; i++)
+			{
+				for (int j = 0; j < 7; j++)
+				{
+					a[i][j] = b[i][j];
+				}
+			}
+			print_page(a);
+		}
+
+		else if (choice == 3) // 위
+		{
+			for (int i = 0; i < 7; i++)
+			{
+				for (int j = 0; j < 7; j++)
+				{
+					b[i][j] = a[(i+1) % 7][j];
+				}
+			}
+
+			for (int i = 0; i < 7; i++)
+			{
+				for (int j = 0; j < 7; j++)
+				{
+					a[i][j] = b[i][j];
+				}
+			}
+			print_page(a);
+		}
+
+		else if (choice == 4) // 아래
+		{
+			for (int i = 0; i < 7; i++)
+			{
+				for (int j = 0; j < 7; j++)
+				{
+					b[(i + 1) % 7][j] = a[i][j];
+				}
+			}
+
+			for (int i = 0; i < 7; i++)
+			{
+				for (int j = 0; j < 7; j++)
+				{
+					a[i][j] = b[i][j];
+				}
+			}
+			print_page(a);
+		}
+
+		scanf("%d", &choice);
+	}
+}
---
+++ 2023.01.12/(2023.01.12) 4기 문찬혁 포인터 기초 활용.cpp
@@ -0,0 +1,103 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void convert(double* grades, double* scores, int size);
+void insert(double *grades);
+void print1(double* scores, int size);
+void print2(int* c, int size);
+void merge(int* a, int* b, int* c, int size);
+
+int main()
+{
+	int size = 10;
+	double grades[10];
+	double scores[10];
+
+	int a[] = { 4, 6, 8, 13, 15 };
+	int b[] = { 1, 3, 5, 7, 10 };
+	int c[10] = { 0 };
+
+	insert(grades);
+	convert(grades, scores, size);
+	print1(scores, size);
+	merge(a, b, c, size);
+	print2(c, size);
+}
+
+void insert(double *grades)
+{
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		grades[i] = rand() % 101;
+	}
+}
+
+void convert(double* grades, double* scores, int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+	{
+		scores[i] = grades[i] * 4.3 / 100;
+	}
+}
+
+void print1(double* scores, int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+	{
+		printf("학생 성적 = %lf\n", scores[i]);
+	}
+}
+
+void print2(int* c, int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+	{
+		printf("merge = %d\n", c[i]);
+	}
+}
+void merge(int* a, int* b, int* c, int size)
+{
+	int i = 0, j = 0, z = 0;
+
+	while (c[9] > 0);
+	{
+		if (a[i] > b[j])
+		{
+			c[z] = b[j];
+			z++;
+			j++;
+		}
+
+		if (a[i] < b[j])
+		{
+			c[z] = a[i];
+			z++;
+			i++;
+		}
+
+		if (a[4] != 0)
+		{
+			for (j; j < 5; j++)
+			{
+				c[z] = b[j];
+				z++;
+			}
+		}
+
+		if (b[4] != 0)
+		{
+			for (i; i < 5; i++)
+			{
+				c[z] = a[i];
+				z++;
+			}
+		}
+
+	}
+	
+}
