317b98793745b1b0c786c287525f6e2c4de11e26
macksgur0303 <121844168+macksgur0303@users.noreply.github.com>
2023-05-17 21:08:19 +0900

    Add files via upload

---
+++ 2023.05.17(05.11)/answer1.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int num = 0, cnt = 0;
+
+	printf("정수를 입력하시오: ");
+	scanf("%d", &num);
+	for (int i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+			cnt++;
+	}
+
+	if (cnt > 0)
+		printf("소수가 아닙니다");
+	else
+		printf("소수입니다");
+}
---
+++ 2023.05.17(05.11)/answer2.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	double num;
+	int mod, cnt = 0;
+
+	printf("실수를 입력하시오(m): ");
+	scanf("%lf", &num);
+
+	printf("모드를 입력하시오(cm == 1, mile == 2, yd == 3, inch == 4): ");
+	scanf("%d", &mod);
+
+	switch (mod)
+	{
+	case 1:
+		num = num * 100;
+		break;
+
+	case 2:
+		num = num * 0.000621371;
+		break;
+
+	case 3:
+		num = num * 1.09361;
+		break;
+
+	case 4:
+		num = num * 39.3701;
+		break;
+
+	default:
+		printf("잘못된 입력 다시 입력하시오\n");
+		cnt++;
+		break;
+	}
+
+	if(cnt == 0)
+		printf("변환된 값 %lf", num);
+}
