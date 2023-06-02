42648a7f94561e243254219496446da709f52da8
macksgur0303 <121844168+macksgur0303@users.noreply.github.com>
2023-06-02 16:58:21 +0900

    Add files via upload

---
+++ 2023.06.02/sort.cpp
@@ -0,0 +1,67 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 30
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    int list[SIZE] = { 0 };
+    int temp;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        list[i] = rand() % 100;
+        printf("%d ", list[i]);
+        if (i == 50)
+            printf("\n");
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < SIZE - 1; i++)
+    {
+        for (int j = i + 1; j < SIZE; j++)
+        {
+            if (list[j] < list[i])
+            {
+                temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+
+        }
+    }
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("%d ", list[i]);
+        if (i == 50)
+            printf("\n");
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < SIZE - 1; i++)
+    {
+        for (int j = i + 1; j < SIZE; j++)
+        {
+            if (list[j] > list[i])
+            {
+                temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+
+        }
+    }
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("%d ", list[i]);
+        if (i == 50)
+            printf("\n");
+    }
+
+}
---
+++ 2023.06.02/sort2.cpp
@@ -0,0 +1,64 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 30
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    int list[SIZE] = { 0 };
+    int temp, a;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        list[i] = rand() % 100;
+        a = rand() % 2;
+        if (a == 1)
+            list[i] = -list[i];
+        printf("%d ", list[i]);
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < SIZE - 1; i++)
+    {
+        for (int j = i + 1; j < SIZE; j++)
+        {
+            if (list[j] < list[i])
+            {
+                temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+
+        }
+    }
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("%d ", list[i]);
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < SIZE - 1; i++)
+    {
+        for (int j = i + 1; j < SIZE; j++)
+        {
+            if (list[j] > list[i])
+            {
+                temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+
+        }
+    }
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("%d ", list[i]);
+    }
+
+}
