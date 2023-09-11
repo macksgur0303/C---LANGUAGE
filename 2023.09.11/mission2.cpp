74749e2efc30850e59dee13f641cee4da2c71670
macksgur0303 <121844168+macksgur0303@users.noreply.github.com>
2023-09-11 22:36:19 +0900

    Add files via upload

---
+++ 2023.09.11/mission2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void* function_A(void* num)
+{
+	int i = 0;
+	for(i = 0; i < 50; i++)
+	{
+		printf(" thread A   i = [%3d]\n", i);
+		usleep(100);
+	}
+
+	pthread_exit(NULL);
+}
+
+void* function_B(void* num)
+{
+	int i = 0;
+	while (1)
+	{
+		printf(" thread A   i = [%3d]\n", i);
+		i++;
+		i = i % 100;
+		sleep(2);
+	}
+}
+
+int main()
+{
+	pthread_t pthread_A, pthread_B;
+	int cnt = 0;
+
+	printf("Create Thread A \n");
+	pthread_create(&pthread_A, NULL, function_A, NULL);
+
+	printf("Create Thread B \n");
+	pthread_create(&pthread_B, NULL, function_B, NULL);
+
+	pthread_join(pthread_A, NULL);
+	//pthread_join(pthread_B, NULL);
+
+	while (1)
+	{
+		printf("Thread test : %3d ", cnt);
+		cnt++;
+		cnt = cnt % 100;
+	}
+	return 1;
+}
